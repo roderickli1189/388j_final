7cc788455fb25067f1628a870275b128e5028ec2
abachhu <ankith.bachhu@gmail.com>
2024-04-17 22:08:04 -0400

    update

--- project_code/flask_app/movies/routes.py
+++ project_code/flask_app/movies/routes.py
@@ -1,7 +1,7 @@
 import base64,io
 from io import BytesIO
-from flask import Blueprint, render_template, url_for, redirect, request, flash
 from flask_login import current_user
+from flask import Blueprint, render_template, url_for, redirect, request, flash
 
 from .. import movie_client
 from ..forms import MovieReviewForm, SearchForm
