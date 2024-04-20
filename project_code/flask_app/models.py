1c196384746fdb744279336d4369b87cc33889ba
Roderick Li <roderickli1189@gmail.com>
2024-04-20 17:59:19 -0400

    quickly changed our models.py

--- project_code/flask_app/models.py
+++ project_code/flask_app/models.py
@@ -2,32 +2,25 @@ from flask_login import UserMixin
 from datetime import datetime
 from . import db, login_manager
 
-
-# TODO: implement
 @login_manager.user_loader
 def load_user(user_id):
     return User.objects(username = user_id).first()
 
-# TODO: implement fields
+# model for User profile
 class User(db.Document, UserMixin):
     username = db.StringField(unique=True, required=True, min=1, max=40)
     email = db.StringField(unique=True, required=True)
     password = db.StringField(required=True)
+    profile_pic = db.ImageField()
 
     # Returns unique string identifying our object
     def get_id(self):
-        # TODO: implement
         return self.username
 
+# model for the post containing image of squirrel and comments
 class SquirrelPost(db.Document):
-    posted_images = db.ImageField()
-
-# TODO: implement fields
-class Review(db.Document):
     commenter = db.ReferenceField(User, required=True)
     content = db.StringField(required=True, min=5, max=500)
     date = db.StringField(required=True)
-    imdb_id = db.StringField(required=True, min=9, max=9)
-    movie_title = db.StringField(required=True, min=1, max=100)
+    location = db.StringField(required=True)
     image = db.StringField()
-    #Uncomment when other fields are ready for review pictures
