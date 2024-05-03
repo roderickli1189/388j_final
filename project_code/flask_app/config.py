83dfe321910c245072b8f5b175c548be2428a084
Roderick Li <roderickli1189@gmail.com>
2024-05-03 15:51:37 -0400

    env set up

--- project_code/flask_app/config.py
+++ project_code/flask_app/config.py
@@ -1,3 +1,4 @@
 # Stores all configuration values
-SECRET_KEY = b'\x81Eu\xa3\x96\xa5^D\x95\xea\n\x10\x0b\x85\xe4\xaf'
-MONGODB_HOST = 'mongodb+srv://rzli:poop@cluster0.d7oqbov.mongodb.net/Cluster0?retryWrites=true&w=majority&appName=Cluster0'
+import os
+SECRET_KEY = os.environ.get('SECRET_KEY')
+MONGODB_HOST = os.environ.get('MONGODB_HOST')
